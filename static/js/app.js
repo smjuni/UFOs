89358b6fa42d8ed35161ae68b589992ca06099fe
smjuni <stephaniejuniper@hotmail.com>
2021-01-15 19:04:26 -0800

    table not filtering

--- static/js/app.js
+++ static/js/app.js
@@ -31,52 +31,45 @@ function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
     let changedElement = d3.select(this);
-    console.log(changedElement);
+    console.log(changedElement); //doesnt show with other prints
 
     // 4b. Save the value that was changed as a variable.
     let elementValue = changedElement.property("value");
-    console.log(elementValue)
+    console.log(elementValue); 
+
     // 4c. Save the id of the filter that was changed as a variable.
     let elementId = changedElement.attr("id");
-
+    console.log(elementId);
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (elementValue) {
       filters[elementId]=elementValue;
     }
     else {
-      delete filters(elementId)
+      delete filters[elementId];
     }
  
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
 
-   
-  
   // 7. Use this function to filter the table when data is entered.
-  function filterTable() 
-  
-  
-
+  function filterTable() {
 
     // 8. Set the filtered data to the tableData.
     let filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    if (filters){
-    filteredData = filteredData.filter(row => elementId === changedElement);
-    
-
-      
-  };
+    if (elementValue){
+    filteredData = filteredData.filter(row => row.elementId === elementValue);
+    };
   
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
 
-  
-  }
-  
+  };
+
+} 
   // 2. Attach an event to listen for changes to each filter
   d3.selectAll("input").on("change", updateFilters);
   
